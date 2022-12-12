9c2b167b81f4aadd685aa9f9f44f5ddddad14bd9
emartinez <enri.fc.98@gmail.com>
2022-12-12 23:22:11 +0100

    ADD: new structure to read map, LEAKING atm!

--- inc/map.h
+++ inc/map.h
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 19:13:14 by franmart          #+#    #+#             */
-/*   Updated: 2022/12/11 20:47:53 by franmart         ###   ########.fr       */
+/*   Updated: 2022/12/12 22:01:33 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,22 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 
-typedef	struct	s_map {
-	int	width;
-	int	height;
-	int	**map;
+typedef struct s_point
+{
+	int				x;
+	int				y;
+	int				z;
+	unsigned int	color;
+} t_point;
+
+typedef	struct	s_map
+{
+	unsigned int	width;
+	unsigned int	height;
+	t_point			**map;
 } t_map;
 
 void	read_file(char *filename, t_map *map);
-int		*read_cols(char *line, t_map *map);
+t_point	*read_cols(char *line, t_map *map, int row);
 
 #	endif
--- src/map.c
+++ src/map.c
@@ -6,62 +6,112 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 20:23:19 by franmart          #+#    #+#             */
-/*   Updated: 2022/12/11 21:23:09 by franmart         ###   ########.fr       */
+/*   Updated: 2022/12/12 23:21:35 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/libft/include/libft.h"
 #include "../inc/map.h"
+#define DEFAULT_COLOR 255
 
-void	read_file(char *filename, t_map *map)
+void	count_lines(char *filename, t_map *map)
 {
-	int		fd;
-	char	*line;
-	int		*col;
+	unsigned int	i;
+	int				fd;
+	char			*line;
 
+	i = 0;
 	fd = open(filename, O_RDONLY);
 	line = ft_gnl(fd);
 	while (line)
 	{
-		map->height += 1;
-		col = read_cols(line, map);
-		// meter las col en un arr de cols dentro del objeto map
-		free(col);
+		free(line);
+		i++;
+		line = ft_gnl(fd);
+	}
+	free(line);
+	map->height = i;
+	close(fd);
+}
+
+void	read_file(char *filename, t_map *map)
+{
+	int		fd;
+	int		row;
+	char	*line;
+	char	*trim_line;
+
+	row = 0;
+	count_lines(filename, map);
+	map->map = ft_calloc(map->height, sizeof(t_point)); // aqui también
+	fd = open(filename, O_RDONLY);
+	line = ft_gnl(fd);
+	while (line)
+	{
+		trim_line = ft_strtrim(line, " \n");
+		free(line);
+		map->map[row] = read_cols(trim_line, map, row); // leak aqui
+		free(trim_line);
+		row++;
 		line = ft_gnl(fd);
 	}
 	free(line);
 	close(fd);
-	ft_printf("Tamaño mapa: %dx%d\n", map->height, map->width);
 }
 
-int	*read_cols(char *line, t_map *map)
+// esta no leakea
+void	read_cell(char *line, t_point *point)
 {
-	char	**arr;
-	char	*clean_line;
-	int		*cols;
+	char	**str;
+
+	if (ft_strchr(line, ',') != 0)
+	{
+		str = ft_split(line, ',');
+		point->y = ft_atoi(str[0]);
+		free(str[0]);
+		point->color = ft_atoi_base(str[1], "0123456789abcdef");
+		free(str[1]);
+		free(str);
+	}
+	else
+	{
+		str = 0;
+		point->y = ft_atoi(line);
+		point->color = DEFAULT_COLOR;
+	}
+}
+
+t_point	*read_cols(char *line, t_map *map, int row)
+{
+	char	**text_cells;
+	t_point	*cols;
 	int		i;
 
 	i = 0;
-	clean_line = ft_strtrim(line, "\n ");
-	free(line);
-	arr = ft_split(clean_line, ' ');
-	while (arr[i])
+	text_cells = ft_split(line, ' ');
+	while (text_cells[i])
 		i++;
-	map->width = i;
-	cols = ft_calloc(i, sizeof(int));
+	if (row == 0)
+		map->width = i;
+	if (map->width != i)
+		exit(1);
+	//ft_printf("line: %s width: %d\n", line, map->width);
+	cols = ft_calloc(i, sizeof(t_point)); // leak aqui de 10 elementos
+	ft_printf("Elementos: %d\n", i);
 	i = 0;
-	while (arr[i])
+	while (text_cells[i])
 	{
-		/* TODO: hay que reescribir esta mierda otra vez porque nadie me avisó
-			de que pueden añadir los colores al mapa XD */
-		if (ft_strchr(arr[i], ','))
-			exit(1);
-		cols[i] = ft_atoi(arr[i]);
-		free(arr[i]);
+		cols[i].x = i;
+		cols[i].z = row;
+		read_cell(line, &cols[i]);
+		ft_printf("Free n%d\n", i);
+		free(text_cells[i]);
+		//ft_printf("celda x:%d\ty:%d\tz:%d\tcolor:%u\n", cols[i].x, cols[i].y, cols[i].z, cols[i].color);
 		i++;
 	}
-	free(arr);
-	free(clean_line);
+	ft_printf("Free n%d\n", i);
+	free(text_cells[i]);
+	free(text_cells);
 	return (cols);
 }
 
@@ -69,7 +119,6 @@ int	main(int argc, char **argv)
 {
 	t_map	map;
 
-	map.height = 0;
 	map.width = 0;
 	read_file(argv[1], &map);
 	return (0);
